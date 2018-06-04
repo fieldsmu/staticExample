980cd87e2204e0ad6193c8d47ca1a0b5394963c3
Marcus Fields <fieldsmu@gmail.com>
2018-06-04 11:43:00 -0400

    first commit

--- staticExample/Program.cs
+++ staticExample/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace staticExample {
 	class Program {
 		static void Main(string[] args) {
+
+			Technology Git = new Technology {
+				Name = "Git/GitHub",
+				Difficulty = 5
+			};
+			Technology.Counter = 1;
+
+			Technology SQL = new Technology {
+				Name = "SQL Server",
+				Difficulty = 4
+			};
 		}
 	}
 }
---
+++ staticExample/Technology.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticExample {
+
+	class Technology {
+		public static int Counter { get; set; }
+		public string Name { get; set; }
+		public int Difficulty { get; set; }
+	}
+}
